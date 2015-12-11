b256af6e4ba60026a3a0f8b9bfe4993f9944f484
Leonardo Chirivì <leonardochirivi1@gmail.com>
2015-12-11 23:33:32 +0100

    Project Euler Java solutions

--- projectEuler/euler10.java
+++ projectEuler/euler10.java
@@ -18,13 +18,12 @@ public class euler10 {
 	public static void main(String[] args) {
 		
 		BigInteger sum = BigInteger.valueOf(0);
-		
+	
 		for ( int i=2 ; i<=2000000;i++ )
 			if (isPrime(i)) sum = sum.add(BigInteger.valueOf(i));
-					
+		
 		//142913828922
 		System.out.println(sum);
 		
 	}
 }
-
